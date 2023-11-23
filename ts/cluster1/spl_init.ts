9b7d0ab5e000d619d5d31ef345b4132e2e465f33
Docho <docho@Dochos-MacBook-Pro.local>
2023-11-23 07:52:49 -0800

    readme updates.

--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,7 +1,6 @@
 import { Keypair, Connection, Commitment } from "@solana/web3.js";
 import { createMint } from '@solana/spl-token';
 import wallet from "../wba-wallet.json"
-//PK 5xb4XeuuCSETmUTeZE3JcPdVqxd6jfZsSNYVNt2BUJYzqzQ2YXiXT5uodnxC5e44Q7xKEXqqGkBSpnaH6TYEvuew
 //mint id GXWDj8NGR7GAH7Rm1i1MLCkC6NmZNh68FBKhPZJm5mQ1
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
