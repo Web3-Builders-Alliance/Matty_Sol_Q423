639944fb90e99381738bb88d705e05ce733c6508
Docho <docho@Dochos-MBP.attlocal.net>
2024-01-02 16:24:51 -0800

    Paul escrow

---
+++ rs/cluster2/paulx-escrow/src/lib.rs
@@ -0,0 +1,9 @@
+// inside lib.rs, only the following line should be in here
+
+//#[cfg(not(feature = "no-entrypoint"))]
+pub mod entrypoint;
+
+pub mod error;
+pub mod instruction;
+pub mod processor;
+pub mod state;
